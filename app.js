d1921db78cddcc69cb10f7d51fddad787f784835
GiorgosTsi <giorgostsikri@hotmail.com>
2024-10-12 19:06:05 +0300

    Add functionality for clear cart button

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ class UI {
                     <img src=${product.image} alt="product" class="product-img">
                     <button class="bag-btn" data-id=${product.id}>
                         <i class="fa fa-shopping-cart"></i>
-                        add to bag
+                        add to cart
                     </button>
                 </div>
                 <h3 >${product.title}</h3>
@@ -145,11 +145,88 @@ class UI {
     showCart(){
         //to make visible the cart just change visibility to cart overlay
         cartOverlay.classList.add('transparentBcg');//transparentBcg cart just make visibility to visible
-        cartDOM.classList.add('showCart')
+        cartDOM.classList.add('showCart');
     }
 
     setupAPP(){
+        /*Method to be used when the dom is loaded.
+        * Get the cart from the local storage if it exists,
+        * so the items in the cart will not dissapear when page is reloaded.
+        */
+        //
+        cart = Storage.getCart();
+        //set the cart values
+        this.setCartValues(cart);
+        //Construct again the cart page:
+        this.populateCart(cart);
+        //add event listener to cart  btn
+        cartBtn.addEventListener('click',this.showCart);
+        //add evemt listener to close btn
+        closeCartBtn.addEventListener('click',this.hideCart);
+
+    }
+
+    hideCart(){
+        cartOverlay.classList.remove('transparentBcg');
+        cartDOM.classList.remove('showCart');
+    }
+
+    populateCart(cart){
+        cart.forEach( item => this.updateCartContent(item)); 
+    }
+
+    cartLogic(){
+        /*Method to add all event listeners for cart logic(remove,increase,decrease) */
+
+
+        /*
+        ** First Line (this.clearCart): The this context inside the clearCart method 
+        might not point to the class instance because the function reference is passed directly,
+        and JavaScript binds this to the DOM element that triggered the event (in this case, clearCartBtn).
+
+        ** Second Line (() => { this.clearCart(); }): The arrow function ensures that
+        the this context remains tied to the class instance because it inherits the lexical
+        this from the scope where the arrow function is defined. This is generally safer in class-based event handling.
+
+        We need the 'this' reference to the class instance (because we gonna use it inside the clearCart)
+        so we use the second way.
+        */
+        //clearCartBtn.addEventListener('click',this.clearCart);
+        clearCartBtn.addEventListener('click',() => { this.clearCart(); });
+    }
+
+    clearCart(){
+        let cartItems = cart.map( item => item.id);
+        cartItems.forEach( itemID => {
+            this.removeItem(itemID);
+        });
+        while( cartContent.children.length > 0){
+            cartContent.removeChild(cartContent.children[0]);
+        }
+        this.hideCart();
+
+    }
+
+    removeItem(id){
+        cart = cart.filter( item => item.id !== id); // keep only the items that satisfy the predicate
         
+        //remove also the item from cart Content DOM
+        //cartContent.removeChild()
+        //store the new cart list in local storage:
+        Storage.saveCart(cart);
+
+        //reset the add to cart button of this item:
+        let button = this.getSingleButton(id);
+        button.disabled = false;
+        button.innerHTML = `<i class="fa fa-shopping-cart"></i>add to cart`;
+
+        //update total value in cart
+        this.setCartValues(cart); //set again #items and total cost.
+    }
+
+    getSingleButton(id){
+        /*Get the product add to cart button by id */
+        return buttonsDOM.find( button => button.dataset.id === id);
     }
 }
 
@@ -172,6 +249,10 @@ class Storage{
     static saveCart(cart){
         localStorage.setItem('cart',JSON.stringify(cart));
     }
+
+    static getCart(){
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    }
 }
 
 
@@ -181,6 +262,9 @@ document.addEventListener("DOMContentLoaded" , ()=>{
     const ui = new UI();
     const products = new Products();
 
+    /*setupAPP method is used, if cart content in stored in local storage,to setup the app */
+    ui.setupAPP();
+
     //get all products from json and then display them in main page.
     products.getProducts().then(products => {
         ui.displayProducts(products);
@@ -189,7 +273,10 @@ document.addEventListener("DOMContentLoaded" , ()=>{
         Storage.saveProducts(products);
     }
     ).then(()=>{
-        ui.getBagButtons()
+        ui.getBagButtons();
+
+        //add event listeners to cart buttons
+        ui.cartLogic();
 
     });
 
